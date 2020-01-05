e2ed2da7899fa5e7beec5037e3923c87b55fe653
unknown <hoangle.3979@gmail.com>
2020-01-05 21:35:08 +0700

    Update api folder

---
+++ src/api/AuthApi.ts
@@ -0,0 +1,8 @@
+import request from "./config/request"
+import {AUTH_URL} from "./config/urls"
+
+const AuthApi = {
+    login: (email: string, password: string) => request.post(AUTH_URL.login, {email, password}),
+    refreshToken: (storedRefreshToken: string) => request.post(AUTH_URL.refreshToken, {storedRefreshToken}),
+}
+export default AuthApi
---
+++ src/api/UserApi.ts
@@ -0,0 +1,6 @@
+import request from "./config/request"
+import {USER_URL} from "./config/urls"
+
+// const UserApi = {
+// }
+export default UserApi
