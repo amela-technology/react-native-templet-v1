6824e3a3652ac1e49197adb67414d413b221b048
Trịnh Đức Duy <44860282+Duya3fithou@users.noreply.github.com>
2021-01-25 09:35:18 +0700

    update-metrics
    fix height metric android

--- src/assets/metrics.ts
+++ src/assets/metrics.ts
@@ -1,13 +1,21 @@
+/* eslint-disable no-nested-ternary */
 /*
  * platform/application wide metrics for proper styling
  */
 import { Dimensions, Platform } from 'react-native';
-import { initialWindowMetrics } from 'react-native-safe-area-context';
+import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
+import { verticalScale } from 'react-native-size-matters';
 
 const { width, height } = Dimensions.get('window');
 
-const safeTopPadding = initialWindowMetrics?.insets.top || 0;
-const safeBottomPadding = initialWindowMetrics?.insets.bottom || 0;
+const safeTopPaddingPlatform = Platform.OS === 'android' ? verticalScale(15) : StaticSafeAreaInsets.safeAreaInsetsTop;
+const safeTopPadding = safeTopPaddingPlatform === 0 ? verticalScale(30) : safeTopPaddingPlatform;
+const safeBottomPadding =
+    Platform.OS === 'android'
+        ? 0
+        : StaticSafeAreaInsets.safeAreaInsetsBottom === 0
+        ? 20
+        : StaticSafeAreaInsets.safeAreaInsetsBottom;
 const Metrics = {
     navBarHeight: Platform.OS === 'ios' ? 54 : 66,
     screenHeight: width < height ? height : width,
