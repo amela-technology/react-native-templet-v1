53e60fc7e60620179f0df54f8b91bd8a994325d0
Thắng Nguyễn <admin@Thang-Nguyen.local>
2021-03-01 14:14:31 +0700

    new-uspaging

--- src/hooks/usePaging.ts
+++ src/hooks/usePaging.ts
@@ -1,109 +1,90 @@
-import { useRequest, useUnmount } from 'ahooks';
 import axios, { AxiosRequestConfig } from 'axios';
 import { useCallback, useEffect, useState } from 'react';
 import { store } from 'app-redux/store';
-
-const { CancelToken } = axios;
+import { logger } from 'utilities/helper';
 
 const SIZE_LIMIT = 10;
 
 const usePaging = (
     requestPaging: (config: AxiosRequestConfig) => Promise<any>,
-    cacheKey: string,
     initialParams?: any,
     onSuccess?: (data?: any, cbParams?: any) => void,
     onError?: (error: Error, cbParams?: any) => void,
 ) => {
-    const [refreshing, setRefreshing] = useState(false);
-    const [loadingMore, setLoadingMore] = useState(false);
-    const [pageIndex, setPageIndex] = useState(1);
-    const [list, setList] = useState<Array<any>>([]);
-    const [error, setError] = useState<Error | null>();
+    const [pagingData, setPagingData] = useState({
+        refreshing: false,
+        loadingMore: false,
+        pageIndex: 1,
+        list: [],
+        noMore: false,
+    });
     const [params, setParams] = useState<any>(initialParams);
-    const [noMore, setNoMore] = useState(false);
-    const source = CancelToken.source();
     useEffect(() => {
-        if (pageIndex > 1) {
-            setLoadingMore(true);
-        }
-        runRequest(pageIndex, SIZE_LIMIT, params);
-    }, [pageIndex]);
+        runRequest(pagingData.pageIndex, SIZE_LIMIT, params);
+    }, [pagingData.pageIndex]);
     useEffect(() => {
-        if (refreshing) {
-            setPageIndex(1);
-        }
-    }, [refreshing]);
-    useEffect(() => {
-        if (!umiRequest.loading) {
-            onRefresh();
-        }
+        onRefresh();
     }, [params]);
-    const handleOnSuccess = (data: any, cbParams: any) => {
+    const handleOnSuccess = (data: any) => {
         const responseData = data || {};
         const newList: [] = responseData.data || [];
-        if (refreshing) {
-            setList(newList);
+        const tempData = {
+            ...pagingData,
+            noMore: pagingData.pageIndex >= responseData?.totalPages,
+            refreshing: false,
+            loadingMore: false,
+        };
+        if (pagingData.pageIndex === 1) {
+            setPagingData({
+                ...tempData,
+                list: newList,
+            });
         } else if (newList.length > 0) {
-            setList([...list, ...newList]);
+            setPagingData({
+                ...tempData,
+                list: [...pagingData.list, ...newList],
+            });
         }
-        setNoMore(pageIndex >= responseData?.totalPages);
-        setRefreshing(false);
-        setLoadingMore(false);
     };
-
-    const handleOnError = (e: Error, cbParams: any) => {
-        setError(error);
-        setRefreshing(false);
-        setLoadingMore(false);
-        onError?.(e, cbParams);
-    };
-
-    const umiRequest = useRequest(requestPaging, {
-        loadMore: false,
-        manual: true,
-        onSuccess: handleOnSuccess,
-        onError: handleOnError,
-        defaultLoading: true,
-        cacheKey,
-    });
-
     // config request paging
-    const runRequest = (requestPageIndex: number, pageSize?: number, otherParams?: any) => {
-        umiRequest.run({
-            params: {
-                pageIndex: requestPageIndex,
-                pageSize: pageSize || SIZE_LIMIT,
-                ...otherParams,
-            },
-            cancelToken: source.token,
-        });
+    const runRequest = async (requestPageIndex: number, pageSize?: number, otherParams?: any) => {
+        try {
+            const res = await requestPaging({
+                params: {
+                    pageIndex: requestPageIndex,
+                    pageSize: pageSize || SIZE_LIMIT,
+                    ...otherParams,
+                },
+            });
+            handleOnSuccess(res);
+        } catch (error) {
+            setPagingData({ ...pagingData, refreshing: false, loadingMore: false });
+            logger(error);
+        }
     };
-
-    useUnmount(() => {
-        source.cancel('useEffect cleanup...');
-    });
-
     const onRefresh = () => {
-        setRefreshing(true);
+        if (pagingData.pageIndex > 1) {
+            setPagingData({ ...pagingData, refreshing: true, pageIndex: 1 });
+        } else {
+            runRequest(1, SIZE_LIMIT, params);
+        }
     };
 
     const onLoadMore = () => {
-        if (!noMore) {
-            setLoadingMore(true);
-            setPageIndex(pageIndex + 1);
+        if (!pagingData.noMore) {
+            setPagingData({
+                ...pagingData,
+                loadingMore: true,
+                pageIndex: pagingData.pageIndex + 1,
+            });
         }
     };
-
     return {
-        ...umiRequest,
-        list: list?.length ? list : umiRequest?.data?.data,
-        noMore,
-        refreshing,
-        loadingMore,
+        pagingData,
         onRefresh,
         onLoadMore,
         setParams,
-        setList,
+        setPagingData,
     };
 };
 
