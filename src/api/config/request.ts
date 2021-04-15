872e5507888f56ded2349d96d8435400d17655e7
Thắng Nguyễn <admin@Thang-Nguyen.local>
2021-04-15 15:16:56 +0700

    update/request

--- src/api/config/request.ts
+++ src/api/config/request.ts
@@ -2,12 +2,13 @@ import axios from 'axios'
 import Config from 'react-native-config'
 import i18next from 'shared/utilities/i18next'
 import TokenProvider from 'services/authenticate/TokenProvider'
+import NetInfo from '@react-native-community/netinfo'
 import AuthenticateService from 'services/authenticate/AuthenticateService'
-import { AUTH_URL } from 'api/config/urls'
+const AUTH_URL_REFRESH_TOKEN = `${Config.API_URL}auth/request-access-token`
 
 const request = axios.create({
     baseURL: Config.API_URL,
-    timeout: 5000,
+    timeout: 8000,
     headers: { Accept: '*/*' },
 })
 // for multiple requests
@@ -25,108 +26,83 @@ const processQueue = (error: any, token: string | null | undefined = null) => {
 
     failedQueue = []
 }
+
 request.interceptors.request.use(
-    async function (config: any) {
+    async (config: any) => {
         // Do something before api is sent
-        const userToken = await TokenProvider.getToken()
-        if (userToken) {
-            config.headers.Authorization = `Bearer ${userToken}`
+        const token = TokenProvider.getToken()
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
         }
-        // __DEV__ &&
-        //     console.log(
-        //         `%c ${config.method?.toUpperCase()} from ${config.url}:`,
-        //         'background: ' + 'yellow' + '; color: #000',
-        //         config,
-        //     )
+        const data = await NetInfo.fetch()
+        if (!data.isConnected) {
+            throw new Error('common.networkError')
+        }
         return config
     },
-    function (error: any) {
+    (error: any) => {
         // Do something with api error
-        // __DEV__ &&
-        //     console.log(
-        //         `%c FAILED ${error.response.method?.toUpperCase()} from ${error.response.config.url}:`,
-        //         'background: ' + 'red' + '; color: #fff',
-        //         error.response,
-        //     )
         return Promise.reject(error)
     },
 )
+
 request.interceptors.response.use(
-    function (response: any) {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        // __DEV__ &&
-        //     console.log(
-        //         `%c SUCCESS ${response.config.method?.toUpperCase()} from ${response.config.url}:`,
-        //         'background: ' + 'green' + '; color: #fff',
-        //         response.data,
-        //     )
-        return response
+    (response: any) => {
+        return response?.data
     },
-    async function (error: any) {
+    async (error: any) => {
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
         const { response } = error || {}
         const { data } = response || {}
-        const { errorMessage } = data || {}
+        const { errorMessage, errorKey, errorCode } = data || {}
 
-        // __DEV__ &&
-        //     console.log(
-        //         `%c FAILED ${error.config?.method?.toUpperCase()} from ${error?.config?.url}:`,
-        //         'background: ' + 'red' + '; color: #fff',
-        //         error.response,
-        //     )
         const originalRequest = error.config
-        if (errorMessage === 'RefreshToken_NotExist') {
-            // console.log('RefreshToken_NotExist => logout :(')
-            // logout here
-            await AuthenticateService.logOut()
-            return Promise.reject(error)
-        }
         if (
             ((error.response && error.response.status === 401) || errorMessage === 'Token_Expire') &&
             !originalRequest.retry
         ) {
             if (isRefreshing) {
-                return new Promise(function (resolve: any, reject: any) {
-                    failedQueue.push({ resolve, reject })
-                })
-                    .then((token: any) => {
-                        originalRequest.headers.Authorization = `Bearer ${token}`
-                        return request(originalRequest)
+                try {
+                    const queuePromise: any = await new Promise((resolve: any, reject: any) => {
+                        failedQueue.push({ resolve, reject })
                     })
-                    .catch((err: any) => {
-                        return Promise.reject(err)
-                    })
+                    originalRequest.headers.Authorization = `Bearer ${queuePromise.token}`
+                    return request(originalRequest)
+                } catch (err) {
+                    return Promise.reject(err)
+                }
             }
-            // console.log('refreshing token...')
+            console.log('refreshing token...')
             originalRequest.retry = true
             isRefreshing = true
-            const localRefreshToken = await TokenProvider.getRefreshToken()
-            return new Promise(function (resolve: any, reject: any) {
-                // we use pure axios when refreshing token
-                axios
-                    .post(`${Config.API_URL}/${AUTH_URL.refreshToken}`, {
-                        refresh_token: localRefreshToken,
-                    })
-                    .then(async ({ data: response_data }: any) => {
-                        const { token, refreshToken } = response_data
-                        await TokenProvider.setAllNewToken(token, refreshToken)
-                        originalRequest.headers.Authorization = `Bearer ${refreshToken}`
-                        processQueue(null, token)
-                        resolve(request(originalRequest))
-                    })
-                    .catch((err: any) => {
-                        processQueue(err, null)
-                        reject(err)
-                    })
-                    .then(() => {
-                        isRefreshing = false
-                    })
-            })
+            const localRefreshToken = TokenProvider.getRefreshToken()
+            const refreshTokenResponse = await axios
+                .post(AUTH_URL_REFRESH_TOKEN, {
+                    refreshToken: localRefreshToken,
+                })
+                .then((responseToken: any) => {
+                    const { token, refreshToken } = responseToken?.data?.data
+                    TokenProvider.setAllNewToken(token, refreshToken)
+                    originalRequest.headers.Authorization = `Bearer ${token}`
+                    isRefreshing = false
+                    processQueue(null, token)
+                    return request(originalRequest)
+                })
+                .catch((err: any) => {
+                    isRefreshing = false
+                    //405 : Status code refreshToken Expire
+                    if (err.response.status === 405) {
+                        AuthenticateService.logOut()
+                        return
+                    }
+                    processQueue(err, null)
+                    Promise.reject(err)
+                })
         }
         error.message = errorMessage || i18next.t('common.errorMessage.requestFailed')
-        return Promise.reject(error)
+        return Promise.reject(error.message || error)
     },
 )
+
 export default request
