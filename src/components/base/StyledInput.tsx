5368ffa374cf6e562bb734d27e30dd34e8afe031
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-07-07 19:09:02 -0700

    feature/update_styledInput located commonInterface

--- src/components/base/StyledInput.tsx
+++ src/components/base/StyledInput.tsx
@@ -12,7 +12,7 @@ import {
     View,
     ViewStyle,
 } from 'react-native';
-import { autoCompleteType, textContentType } from 'components/common/CommonInterface';
+import { autoCompleteType, textContentType } from 'utilities/CommonInterface';
 import StyledText from './StyledText';
 
 export interface StyledInputProps extends TextInputProps {
