6c8ca9494e86bc5e041c8c1add5fa1ddff4c5916
Dương Quang <duongquang16111997@gmail.com>
2022-06-29 19:52:53 +0700

    feat: optimize request

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,9 +2,9 @@ import axios, { AxiosRequestConfig } from 'axios';
 import Config from 'react-native-config';
 import TokenProvider from 'utilities/authenticate/TokenProvider';
 import AuthenticateService from 'utilities/authenticate/AuthenticateService';
-import { logger, renderAlert } from 'utilities/helper';
+import { renderAlert } from 'utilities/helper';
 import NetInfo from '@react-native-community/netinfo';
-import { apiLogger } from 'utilities/logger';
+import { apiLogger, logger } from 'utilities/logger';
 import { ERRORS } from 'utilities/staticData';
 import i18next from 'utilities/i18next';
 
@@ -16,26 +16,11 @@ const request = axios.create({
     timeout: 8000,
     headers: { Accept: '*/*' },
 });
-// for multiple requests
-let isRefreshing = false;
-let failedQueue: any = [];
 
-const processQueue = (error: any, token: string | null | undefined = null) => {
-    failedQueue.forEach((prom: any) => {
-        if (error) {
-            prom.reject(error);
-        } else {
-            prom.resolve(token);
-        }
-    });
-
-    failedQueue = [];
-};
-
-const rejectError = (err: string, validNetwork: boolean) => {
+const rejectError = (err: any, validNetwork: boolean) => {
     // Avoid being null
     if (validNetwork !== false) {
-        return Promise.reject(i18next.t(err));
+        return Promise.reject(i18next.t(`${err}`));
     }
     return Promise.reject(i18next.t(ERRORS.network));
 };
@@ -90,24 +75,8 @@ request.interceptors.response.use(
             AuthenticateService.logOut();
             return rejectError(error, validNetwork);
         }
-        if (
-            ((error.response && error.response.status === 401) || errorMessage === 'Token_Expire') &&
-            !originalRequest.retry
-        ) {
-            if (isRefreshing) {
-                try {
-                    const queuePromise: any = await new Promise((resolve: any, reject: any) => {
-                        failedQueue.push({ resolve, reject });
-                    });
-                    originalRequest.headers.Authorization = `Bearer ${queuePromise.token}`;
-                    return request(originalRequest);
-                } catch (err) {
-                    return rejectError(String(err), validNetwork);
-                }
-            }
+        if ((error.response && error.response.status === 401) || errorMessage === 'Token_Expire') {
             logger('refreshing token...');
-            originalRequest.retry = true;
-            isRefreshing = true;
             const localRefreshToken = TokenProvider.getRefreshToken();
             try {
                 const refreshTokenResponse = await axios.post(AUTH_URL_REFRESH_TOKEN, {
@@ -116,15 +85,11 @@ request.interceptors.response.use(
                 const { token, refreshToken } = refreshTokenResponse.data;
                 TokenProvider.setAllNewToken(token, refreshToken);
                 originalRequest.headers.Authorization = `Bearer ${token}`;
-                processQueue(null, token);
                 return request(originalRequest);
             } catch (err) {
                 // Logout here
                 AuthenticateService.logOut();
-                processQueue(err, null);
-                return rejectError(String(err), validNetwork);
-            } finally {
-                isRefreshing = false;
+                return rejectError(err, validNetwork);
             }
         }
         error.message = errorMessage || ERRORS.default;
